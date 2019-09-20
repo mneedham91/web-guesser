d5d50862e8f0e643781a44a164c2cb61c7e8895b
Matt Needham <matthew.m.needham@gmail.com>
2019-09-20 12:21:58 -0400

    I4: project complete

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,11 +1,23 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-number = rand(100) + 1
+@@number = rand(100) + 1
+@@guesses_remaining = 5
 
 get '/' do
-	message = check_guess(params['guess'].to_i, number)
-	erb :index, :locals => {:number => number, :message => message}
+	@@guesses_remaining -= 1
+	if @@guesses_remaining == 0
+		@@number = rand(100) + 1
+		@@guesses_remaining = 5
+		message = 'You lost! A new number has been generated.'
+	else
+		message = check_guess(params['guess'].to_i, @@number)
+		if message == "You got it right!"
+			@@number = rand(100) + 1
+			@@guesses_remaining = 5
+		end
+	end
+	erb :index, :locals => {:number => @@number, :message => message, :cheat => params['cheat']}
 end
 
 def check_guess(guess, number)
